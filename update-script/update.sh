d7cc3212c8c5e3690db6094fb47a8af47d747774
Preston Carpenter <APragmaticPlace@gmail.com>
2021-10-21 15:44:48 -0700

    Fixed my username

--- update-script/update.sh
+++ update-script/update.sh
@@ -248,7 +248,7 @@ Maintainers: Amazon Linux <amazon-linux@amazon.com> (@amazonlinux),
              Sean Kelly (@cbgbt),
              Tanu Rampal (@trampal),
              Kyle Gosselin-Harris (@kgharris),
-             Preston Carpenter (@prcarpen)
+             Preston Carpenter (@timidger)
 GitRepo: https://github.com/amazonlinux/container-images.git
 GitCommit: ${COMMIT_FOR_BRANCH[master]}
 
